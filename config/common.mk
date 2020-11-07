443d823793d97cabc83bfe17431f6d28e153a05b
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    build: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.

--- config/common.mk
+++ config/common.mk
@@ -231,7 +231,12 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+        persist.sys.disable_rescue=true
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
+
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/common \
     vendor/lineage/overlay/no-rro
