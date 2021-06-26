5995da336e008c85b3270a91331155df8231ce52
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -225,6 +225,8 @@ endif
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 
