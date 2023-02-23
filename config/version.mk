54ceff1c135096fac63c4043145639bab51a7461
SirRGB <sirrgb@proton.me>
2023-02-23 19:56:50 +0100

    add gapps/foss logic

--- config/version.mk
+++ config/version.mk
@@ -7,9 +7,32 @@ else
     LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
 endif
 
+# Vanilla
+ifeq ($(GAPPS),)
+    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Sweets/ota_config/raw/snowcone/{device}.json
+    WITH_GMS := true
+    SWEET_EXTRAVERSION = VANILLA-
+endif
+
+# Chocolate
+ifeq ($(GAPPS),true)
+    $(GAPPS will be included in the build)
+    SWEET_EXTRAVERSION = CHOCOLATE-
+    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Sweets/ota_config/raw/snowcone_g/{device}.json
+    ifeq ($(GAPPS_ARM32),)
+        $(warning GAPPS_ARM32 is not set, it defaulting to 64 bit)
+        $(warning Dont try to set it, only needed for 32 bit devices)
+        $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+    endif
+    ifeq ($(GAPPS_ARM32), false)
+        $(warning including 32 bit gapps)
+        $(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
+    endif
+endif
+
 # Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
 
-LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(SWEET_EXTRAVERSION)-$(LINEAGE_BUILD)
 
 # Internal version
 LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_VERSION_SUFFIX)
