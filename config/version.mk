baa54ac2d263da5494b7f28716ab35bea47e941b
SirRGB <sirrgb@proton.me>
2023-06-11 17:27:04 +0200

    rename vars to dessert

--- config/version.mk
+++ config/version.mk
@@ -4,16 +4,16 @@ PRODUCT_VERSION_CODENAME = CUPCAKE-WIP
 
 # Vanilla
 ifeq ($(GAPPS),)
-    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Sweets/ota_config/raw/snowcone/{device}.json
+    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Dessert/ota_config/raw/snowcone/{device}.json
     WITH_GMS := true
-    PRODUCT_SWEET_EXTRAVERSION = VANILLA-
+    PRODUCT_DESSERT_EXTRAVERSION = VANILLA-
 else
 
 # Chocolate
 ifeq ($(GAPPS),true)
     $(GAPPS will be included in the build)
-    PRODUCT_SWEET_EXTRAVERSION = CHOCOLATE-
-    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Sweets/ota_config/raw/snowcone_g/{device}.json
+    PRODUCT_DESSERT_EXTRAVERSION = CHOCOLATE-
+    PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/Project-Dessert/ota_config/raw/snowcone_g/{device}.json
     ifeq ($(GAPPS_ARM32),)
         $(warning GAPPS_ARM32 is not set, it defaulting to 64 bit)
         $(warning Dont try to set it, only needed for 32 bit devices)
@@ -27,5 +27,5 @@ endif
 
 
 # Display version
-LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_CODENAME)-$(PRODUCT_SWEET_EXTRAVERSION)$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)
-LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_CODENAME)-$(PRODUCT_SWEET_EXTRAVERSION)$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)
+LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_CODENAME)-$(PRODUCT_DESSERT_EXTRAVERSION)$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)
+LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_CODENAME)-$(PRODUCT_DESSERT_EXTRAVERSION)$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)
